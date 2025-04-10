3d9dab81075d0d8a0cecfbd0b4cc1612ac4f0047
Bruno Lima de Santana <brunosssss@gmail.com>
2025-04-10 00:20:23 -0300

    ajustando funcao para extrair id partida e colocando novas bibliotecas

--- extract/extract_ID.py
+++ extract/extract_ID.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from dotenv import load_dotenv
+import pandas as pd
 
 load_dotenv()
 
@@ -11,5 +12,12 @@ puuid_minerva = os.getenv('puuid_minerva')
 
 
 def id_match ():
-    url = requests.get(f'https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid_minerva}/ids?start=0&count=20&api_key={api_key}')
-    print(url.json())
+    list_idPartida = requests.get(f'https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid_minerva}/ids?start=0&count=20&api_key={api_key}').json()
+    return list_idPartida
+
+
+
+
+
+if __name__ == '__main__':
+    id_match = id_match()
--- main.py
+++ main.py
@@ -2,17 +2,5 @@ import requests
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 if __name__ == '__main__':
     ...
