3cdd3ac39576a38fe5a7be74490e78e4c08c4668
Bruno Lima de Santana <brunosssss@gmail.com>
2025-05-23 15:43:02 -0300

    comando para criacao das tabelas no sqllite

---
+++ SQLLite/streamers.py
@@ -0,0 +1,38 @@
+import sqlite3
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+DB_NAME = 'db.sqlite3'
+DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME_STREAMERS = 'streamers'
+TABLE_NAME_PARTIDAS = 'matches'
+
+connection = sqlite3.connect(DB_FILE)
+cursor = connection.cursor()
+
+# Criação da tabela de streamers
+cursor.execute(
+    f'''
+    CREATE TABLE IF NOT EXISTS {TABLE_NAME_STREAMERS} (
+        id_streamer INTEGER PRIMARY KEY AUTOINCREMENT,
+        nick_name TEXT,
+        puuid TEXT
+    )
+    '''
+)
+
+# Criação da tabela de partidas
+cursor.execute(
+    f'''
+    CREATE TABLE IF NOT EXISTS {TABLE_NAME_PARTIDAS} (
+        id_partida INTEGER PRIMARY KEY AUTOINCREMENT,
+        id_streamer INTEGER,
+        id_partidas_lol TEXT,
+        FOREIGN KEY(id_streamer) REFERENCES {TABLE_NAME_STREAMERS}(id_streamer)
+    )
+    '''
+)
+
+connection.commit()
+cursor.close()
+connection.close()
